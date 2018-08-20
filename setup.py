0f05756985e35451ffe4ebdc30df689b7560829c
Chris Kimmel <chriskimmel@gmail.com>
2018-08-19 19:35:08 -0700

    added clean command for deleting build files

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from setuptools import setup
+import os
+from setuptools import setup, Command
+
+
+class Clean(Command):
+    description = "Cleans up the build files"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            os.system('rm -rf build dist toil.egg-info')
+            print("Removed build, dist, and toil.egg-info directories")
+        except Exception as e:
+            raise e
+
 
 setup(
     name='toil',
@@ -14,5 +34,8 @@ setup(
     },
     py_modules=['toil'],
     packages=['toil_lib'],
-    install_requires=['cement', 'python-dateutil']
+    install_requires=['cement', 'python-dateutil'],
+    cmdclass={
+        'clean': Clean
+    }
 )
