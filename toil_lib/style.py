9e5abad86a8152667debace11fe3230e93c171bc
Chris Kimmel <chriskimmel@gmail.com>
2018-08-27 13:58:02 -0700

    added colorizers

--- toil_lib/style.py
+++ toil_lib/style.py
@@ -1,3 +1,4 @@
+# styles
 def underline(string):
     return "\033[4m" + string + "\033[0m"
 
@@ -6,17 +7,56 @@ def bright(string):
     return "\033[1m" + string + "\033[0m"
 
 
-def white(string):
-    return "\033[37m " + string + "\033[0m"
+def dim(string):
+    return "\033[2m" + string + "\033[0m"
+
+
+# colors
+def dark(string):
+    return "\033[30m" + string + "\033[0m"
+
+
+def red(string):
+    return "\033[31m" + string + "\033[0m"
 
 
 def green(string):
     return "\033[32m" + string + "\033[0m"
 
 
-def gold(string):
-    return "\033[1m\033[36m" + string + "\033[0m"
+def yellow(string):
+    return "\033[33m" + string + "\033[0m"
 
 
-def supergreen(string):
-    return "\033[37m\033[32m" + string + "\033[0m"
+def blue(string):
+    return "\033[34m" + string + "\033[0m"
+
+
+def cyan(string):
+    return "\033[36m" + string + "\033[0m"
+
+
+def white(string):
+    return "\033[37m " + string + "\033[0m"
+
+
+# brights
+def brightgreen(string):
+    return "\033[32;1m" + string + "\033[0m"
+
+
+def brightyellow(string):
+    return "\033[33;1m" + string + "\033[0m"
+
+
+def brightred(string):
+    return "\033[91" + string + "\033[0m"
+
+
+def brightblue(string):
+    return "\033[94" + string + "\033[0m"
+
+
+# custom
+def tagline(string):
+    return "\033[36;4m" + string + "\033[0m"
